92ed372d4e2a121cd714944ec28c0d5eaaa1209c
Jeremy J. Miller <jeremy.miller2@e-hps.com>
2014-10-30 10:24:21 -0400

    fixes to allow this to work

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,18 +11,18 @@ logstash ||= {}
 if logstash['host']
   include_recipe "chef_handler"
 
-  cookbook_file "#{node['chef_handler']['handler_path']}/chef-logstash-notify.rb" do
+  cookbook_file "#{Chef::Config[:file_cache_path]}/chef-logstash-notify.rb" do
     source "chef-logstash-notify.rb"
-  end.run_action(:create)
+  end
 
   chef_handler "LogStashNotify" do
-    source "#{node['chef_handler']['handler_path']}/chef-logstash-notify.rb"
+    source "#{Chef::Config[:file_cache_path]}/chef-logstash-notify.rb"
     arguments [
       :host => logstash['host'],
       :port => logstash['port'],
       :unique_message => logstash['unique_message']
     ]
-    supports :exception => true
+    supports :exception => true, :report => true
     action :nothing
-  end.run_action(:enable)
+  end
 end
