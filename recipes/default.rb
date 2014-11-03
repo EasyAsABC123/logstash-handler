d3b001ff101d6fb64038470c16b3f238a06a14e3
Justin Schuhmann <justin.schuhmann@e-hps.com>
2014-11-03 14:58:48 -0500

    changed slightly to use chef_handler
    #{node['chef_handler']['handler_path']}

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,15 +9,15 @@ logstash = node['chef_client']['handler']['logstash'] if node['chef_client'] && 
 logstash ||= {}
 
 if logstash['host']
-  include_recipe "chef_handler"
+  include_recipe "chef_handler::default"
 
-  cookbook_file "#{Chef::Config[:file_cache_path]}/chef-logstash-notify.rb" do
+  cookbook_file "#{node['chef_handler']['handler_path']}/chef-logstash-notify.rb" do
     source "chef-logstash-notify.rb"
     action :create
   end
 
-  chef_handler "::LogStashNotifyModule::LogStashNotify" do
-    source "#{Chef::Config[:file_cache_path]}/chef-logstash-notify.rb"
+  chef_handler "LogStashNotifyModule::LogStashNotify" do
+    source "#{node['chef_handler']['handler_path']}/chef-logstash-notify.rb"
     arguments [
       :host => logstash['host'],
       :port => logstash['port'],
