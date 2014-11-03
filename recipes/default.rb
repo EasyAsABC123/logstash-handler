d2f7825c15e8b13e37525270eeba2b958096ae57
Justin Schuhmann <justin.schuhmann@e-hps.com>
2014-11-03 16:49:34 -0500

    FIXED ISSUE WITH CONSTANT

--- recipes/default.rb
+++ recipes/default.rb
@@ -12,15 +12,33 @@ if logstash['host']
   include_recipe 'chef_handler'
 
   handler_path = node['chef_handler']['handler_path']
-  handler = ::File.join handler_path, 'chef-logstash-notify'
+  handler = ::File.join handler_path, 'chef_logstash_notify'
+  Chef::Log::info("#{handler}.rb")
 
   cookbook_file "#{handler}.rb" do
-    source 'chef-logstash-notify.rb'
+    source 'chef_logstash_notify.rb'
     action :create
+  end
+
+  ##
+  # This was primarily done to prevent others from having to stub
+  # `include_recipe "reboot_handler"` inside ChefSpec.  ChefSpec
+  # doesn't seem to handle the following well on convergence.
+  begin
+    require handler
+  rescue LoadError
+    log 'Unable to require the LogStash handler!' do
+      action :write
+    end
   end
 
   chef_handler 'LogStash::LogStashNotify' do
     source handler
+    arguments [
+      :host => logstash['host'],
+      :port => logstash['port'],
+      :unique_message => logstash['unique_message']
+    ]
     supports :report=>true, :exception=>true
     action :enable
   end
