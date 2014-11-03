769012ceeb39343354f56c931afc201e971067c7
Justin Schuhmann <justin.schuhmann@e-hps.com>
2014-11-03 15:03:17 -0500

    tinkering with minor changes to attempt to resolve

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'jmschu02@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures logstash-handler'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.1.3'
+version          '1.1.4'
--- recipes/default.rb
+++ recipes/default.rb
@@ -9,15 +9,15 @@ logstash = node['chef_client']['handler']['logstash'] if node['chef_client'] && 
 logstash ||= {}
 
 if logstash['host']
-  include_recipe "chef_handler::default"
+  include_recipe 'chef_handler::default'
 
   cookbook_file "#{node['chef_handler']['handler_path']}/chef-logstash-notify.rb" do
-    source "chef-logstash-notify.rb"
+    source 'chef-logstash-notify.rb'
     action :create
   end
 
-  chef_handler "LogStashNotifyModule::LogStashNotify" do
-    source "#{node['chef_handler']['handler_path']}/chef-logstash-notify.rb"
+  chef_handler 'LogStashNotifyModule::LogStashNotify' do
+    source "#{node['chef_handler']['handler_path']}/chef-logstash-notify"
     arguments [
       :host => logstash['host'],
       :port => logstash['port'],
